7c5e6ba7780b88a90eb711a53e37184c9f7e1f29
Jorge Matos <jisapjisap@gmail.com>
2024-11-18 16:20:18 +0100

    Navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -11,15 +11,24 @@ const navLinks = [
 
 export default function Navbar() {
 	return (
-		<section className="py-24">
-			<div className="container">
-				<div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
+		<section className="py-24 lg:py-8">
+			<div className="container max-w-5xl">
+				<div className="grid grid-cols-2 lg:grid-cols-3 border border-white/15 rounded-full p-2 px-4 md:pr-2 items-center">
 					<div>
 						<Image
 							src={logoImage}
 							alt="Layers logo"
-							className="h-9 w-auto"
+							className="h-9 w-auto md:h-auto"
 						/>
+					</div>
+					<div>
+						<nav className="flex gap-6 font-medium">
+							{navLinks.map((link) => (
+								<a href={link.href} key={link.label}>
+									{link.label}
+								</a>
+							))}
+						</nav>
 					</div>
 					<div className="flex justify-end gap-4">
 						<svg 
