6215d67080e55bda0d09e964128ba67bafd22390
Jorge Matos <jisapjisap@gmail.com>
2024-11-18 15:38:44 +0100

    Navbar

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -10,14 +10,33 @@ const navLinks = [
 
 export default function Navbar() {
 	return (
-		<section>
+		<section className="py-24">
 			<div className="container">
-				<div className="grid grid-cols-2">
+				<div className="grid grid-cols-2 border border-white/15 rounded-full p-2 px-4 items-center">
 					<div>
 						<Image
 							src={logoImage}
 							alt="Layers logo"
+							className="h-9 w-auto"
 						/>
+					</div>
+					<div className="flex justify-end">
+						<svg 
+							xmlns="http://www.w3.org/2000/svg" 
+							width="24" 
+							height="24" 
+							viewBox="0 0 24 24" 
+							fill="none" 
+							stroke="currentColor" 
+							stroke-width="2" 
+							stroke-linecap="round" 
+							stroke-linejoin="round" 
+							className="feather feather-menu "
+						>
+							<line x1="3" y1="12" x2="21" y2="12"></line>
+							<line x1="3" y1="6" x2="21" y2="6"></line>
+							<line x1="3" y1="18" x2="21" y2="18"></line>
+						</svg>
 					</div>
 				</div>
 			</div>
