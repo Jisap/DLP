d034e5c997cf09344608a0673f8ccba0fb70cf03
Jorge Matos <jisapjisap@gmail.com>
2024-11-20 15:23:36 +0100

    Pointer

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,29 @@
 import Button from "@/components/Button";
+import designExample1Image from "@/assets/images/design-example-1.png";
+import designExample2Image from "@/assets/images/design-example-2.png";
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
 
 
 export default function Hero() {
     return (
-        <section className="py-24">
-					<div className="container">
+        <section className="py-24 overflow-x-clip">
+					<div className="container relative">
+						<div className="absolute -left-32 top-16">
+							<Image 
+								src={designExample1Image}
+								alt="Design Example 1"
+							/>
+						</div>
+					<div className="absolute -right-64 -top-16">
+						<Image
+							src={designExample2Image}
+							alt="Design Example 2"
+						/>
+					</div>
+					<div className="absolute left-56 top-96">
+						<Pointer name="Andrea" />
+					</div>
 						<div className="flex justify-center">
 							<div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
 								✨$7.5M seed round raised
