8d333a7505f1703f4707923842d8ead0a47030c9
Jorge Matos <jisapjisap@gmail.com>
2024-11-20 15:26:12 +0100

    Pointer

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -24,6 +24,9 @@ export default function Hero() {
 					<div className="absolute left-56 top-96">
 						<Pointer name="Andrea" />
 					</div>
+					<div className="absolute right-80 -top-4">
+						<Pointer name="Bryan" color="red" />
+					</div>
 						<div className="flex justify-center">
 							<div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
 								✨$7.5M seed round raised
