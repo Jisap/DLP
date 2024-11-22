9c8a223fd6a6b7ef8893edec82058582fa64e6dc
Jorge Matos <jisapjisap@gmail.com>
2024-11-22 15:26:31 +0100

    features

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -5,6 +5,7 @@ import avatar2 from "@/assets/images/avatar-lula-meyers.jpg"
 import avatar3 from "@/assets/images/avatar-florence-shaw.jpg"
 import Image from "next/image";
 import Avatar from "@/components/Avatar";
+import Key from "@/components/Key";
 
 const features = [
 	"Asset Library",
@@ -78,14 +79,24 @@ export default function Features() {
 					<FeatureCard
 						title="Keyboard Quick Actions"
 						description="Powerful commands to help you create designs more quickly"
-					>			
+					>	
+						<div className="aspect-video flex items-center justify-center gap-4">
+							<Key className="w-28">shift</Key>
+							<Key>alt</Key>
+							<Key>C</Key>
+						</div>		
 					</FeatureCard>
 				</div>
-				<div>
+				<div className="mt-8 flex flex-wrap gap-3 justify-center">
 					{features.map((feature) => (
-						<div key={feature}>
-							<span></span>
-							<span>{feature}</span>
+						<div 
+							key={feature}
+							className="bg-neutral-900 border border-white/10 inline-flex px-3 py-1.5 rounded-2xl gap-3 items-center"	
+						>
+							<span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex items-center justify-center text-xl">
+								&#10038;
+							</span>
+							<span className="font-medium">{feature}</span>
 						</div>
 					))}
 				</div>
