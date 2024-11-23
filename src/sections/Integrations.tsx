ec497d7a3d97c6a82c1e4a8b6d7418266bb239dc
Jorge Matos <jisapjisap@gmail.com>
2024-11-23 14:43:05 +0100

    Integrations

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -1,12 +1,19 @@
 import Tag from "@/components/Tag";
+import figmaIcon from "@/assets/images/figma-logo.svg"
+import notionIcon from "@/assets/images/notion-logo.svg"
+import slackIcon from "@/assets/images/slack-logo.svg"
+import relumeIcon from "@/assets/images/relume-logo.svg"
+import framerIcon from "@/assets/images/framer-logo.svg"
+import githubIcon from "@/assets/images/github-logo.svg"
+import Image from "next/image";
 
 const integrations = [
-	{ name: "Figma", icon: "figma-logo.svg", description: "Figma is a collaborative interface design tool." },
-	{ name: "Notion", icon: "notion-logo.svg", description: "Notion is an all-in-one workspace for notes and docs." },
-	{ name: "Slack", icon: "slack-logo.svg", description: "Slack is a powerful team communication platform." },
-	{ name: "Relume", icon: "relume-logo.svg", description: "Relume is a no-code website builder and design system." },
-	{ name: "Framer", icon: "framer-logo.svg", description: "Framer is a professional website prototyping tool." },
-	{ name: "GitHub", icon: "github-logo.svg", description: "GitHub is the leading platform for code collaboration." },
+	{ name: "Figma", icon: figmaIcon, description: "Figma is a collaborative interface design tool." },
+	{ name: "Notion", icon: notionIcon, description: "Notion is an all-in-one workspace for notes and docs." },
+	{ name: "Slack", icon: slackIcon, description: "Slack is a powerful team communication platform." },
+	{ name: "Relume", icon: relumeIcon, description: "Relume is a no-code website builder and design system." },
+	{ name: "Framer", icon: framerIcon, description: "Framer is a professional website prototyping tool." },
+	{ name: "GitHub", icon: githubIcon, description: "GitHub is the leading platform for code collaboration." },
 ];
 
 export default function Integrations() {
@@ -21,7 +28,13 @@ export default function Integrations() {
 				<div>
 					{integrations.map((integration) => (
 						<div key={integration.name} >
-							<div></div>
+							<div>
+								<Image 
+									src={integration.icon}
+									alt={`${integration.name}`}
+
+								/>
+							</div>
 							<h3>{integration.name}</h3>
 							<p>{integration.description}</p>
 						</div>
