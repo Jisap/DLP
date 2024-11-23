db21f9da9ecb084a282ff2ef910d2be7771f6323
Jorge Matos <jisapjisap@gmail.com>
2024-11-23 16:12:43 +0100

    Integrations

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -23,21 +23,27 @@ export default function Integrations() {
 	return (
 		<section className="py-28 overflow-hidden">
 			<div className="container">
-				<Tag>Integrations</Tag>
-				<h2 className="text-6xl font-medium mt-6">
-					Play well with <span className="text-lime-400">others</span>
-				</h2>
-				<p className="text-white/50 mt-4 text-lg">
-					Layers seamlessly connects with your favorite tools, making it easy to plug into any workflow and collaborate across platforms.
-				</p>
-				<div className="h-[400px] overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]">
-					<IntegrationsColumn 
-						integrations={integrations}
-					/>
-					<IntegrationsColumn 
-						integrations={integrations.slice().reverse()}
-						className="hidden md:flex"
-					/>
+				<div className="grid lg:grid-cols-2 items-center lg:gap-16">
+					<div>
+						<Tag>Integrations</Tag>
+						<h2 className="text-6xl font-medium mt-6">
+							Play well with <span className="text-lime-400">others</span>
+						</h2>
+						<p className="text-white/50 mt-4 text-lg">
+							Layers seamlessly connects with your favorite tools, making it easy to plug into any workflow and collaborate across platforms.
+						</p>
+					</div>
+					<div>
+						<div className="h-[400px] lg:h-[800px] mt-8 lg:mt-0 overflow-hidden grid md:grid-cols-2 gap-4 [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]">
+							<IntegrationsColumn
+								integrations={integrations}
+							/>
+							<IntegrationsColumn
+								integrations={integrations.slice().reverse()}
+								className="hidden md:flex"
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 		</section>
