5820d69d671f9a10707437711964011303526c85
Jorge Matos <jisapjisap@gmail.com>
2024-11-23 15:23:25 +0100

    Integrations

--- src/sections/Integrations.tsx
+++ src/sections/Integrations.tsx
@@ -18,27 +18,34 @@ const integrations = [
 
 export default function Integrations() {
 	return (
-		<section className="py-28 lg:py-40">
+		<section className="py-28 overflow-hidden">
 			<div className="container">
 				<Tag>Integrations</Tag>
-				<h2>Play well with <span>others</span></h2>
-				<p>
-					Layers seamlessly connects with your favorite tools, maling it easy to plug into any workflow and collaborate across platforms.
+				<h2 className="text-6xl font-medium mt-6">
+					Play well with <span className="text-lime-400">others</span>
+				</h2>
+				<p className="text-white/50 mt-4 text-lg">
+					Layers seamlessly connects with your favorite tools, making it easy to plug into any workflow and collaborate across platforms.
 				</p>
-				<div>
-					{integrations.map((integration) => (
-						<div key={integration.name} >
-							<div>
-								<Image 
-									src={integration.icon}
-									alt={`${integration.name}`}
-
-								/>
+				<div className="h-[400px] overflow-hidden [mask-image:linear-gradient(to_bottom,transparent_0%,black_10%,black_90%,transparent_100%)]">
+					<div className="flex flex-col gap-4 pb-4">
+						{integrations.map((integration) => (
+							<div 
+								key={integration.name}
+								className="bg-neutral-900 border border-white/10 rounded-3xl p-6"
+							>
+								<div className="flex justify-center">
+									<Image 
+										src={integration.icon}
+										alt={`${integration.name}`}
+										className="size-24"
+									/>
+								</div>
+								<h3 className="text-3xl text-center mt-6">{integration.name}</h3>
+								<p className="text-center text-white/50 mt-2">{integration.description}</p>
 							</div>
-							<h3>{integration.name}</h3>
-							<p>{integration.description}</p>
-						</div>
-					))}
+						))}
+					</div>
 				</div>
 			</div>
 		</section>
