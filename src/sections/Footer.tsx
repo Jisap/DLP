dab734dabbbbf05149b3b14665131376a0f5ada6
Jorge Matos <jisapjisap@gmail.com>
2024-11-24 11:38:00 +0100

    footer

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,9 +1,33 @@
+import logoImage from "@/assets/images/logo.svg"
+import { footer } from "framer-motion/client";
+import Image from "next/image";
+
 const footerLinks = [
-    { href: "#", label: "Contact" },
-    { href: "#", label: "Privacy Policy" },
-    { href: "#", label: "Terms & Conditions" },
+	{ href: "#", label: "Contact" },
+	{ href: "#", label: "Privacy Policy" },
+	{ href: "#", label: "Terms & Conditions" },
 ];
 
 export default function Footer() {
-    return <div>Footer</div>;
+	return (
+		<section>
+			<div className="container">
+				<div>
+					<div>
+						<Image 
+							src={logoImage}
+							alt="Layers Logo"
+						/>
+					</div>
+					<div>
+						<nav>
+							{footerLinks.map((link) => (
+								<a href={link.href} key={link.label}>{link.label}</a>
+							))}
+						</nav>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
