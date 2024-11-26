21631d6f4c6b43dc4e0d3c03a8951723a8df2bac
Jorge Matos <jisapjisap@gmail.com>
2024-11-26 20:45:53 +0100

    Hero animations

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,17 +13,29 @@ export default function Hero() {
 
 	const [leftDesignScope, leftDesignAnimate] = useAnimate();
 	const [leftPointerScope, leftPointerAnimate] = useAnimate();
+	const [rightDesignScope, rightDesignAnimate] = useAnimate();
+	const [rightPointerScope, rightPointerAnimate] = useAnimate();
 
 	useEffect(() => {
 		leftDesignAnimate([
-			[leftDesignScope.current, {opacity: 1}, {duration: 0.5}],
-			[leftDesignScope.current, {y:0, x:0}, {duration: 0.5}],
+			[leftDesignScope.current, { opacity: 1 }, {duration: 0.5}],
+			[leftDesignScope.current, { y:0, x:0 }, {duration: 0.5}],
 		]);
 		leftPointerAnimate([
-			[leftPointerScope.current, {opacity: 1}, {duration: 0.5}],
-			[leftPointerScope.current, {y:0, x: -100}, {duration: 0.5}],
-			[leftPointerScope.current, { x: 0, y: [0, 16, 0]}, {duration: 0.5, ease: "easeInOut"}],
+			[leftPointerScope.current, { opacity: 1 }, {duration: 0.5}],
+			[leftPointerScope.current, { y:0, x: -100 }, {duration: 0.5}],
+			[leftPointerScope.current, { x: 0, y: [0, 16, 0] }, {duration: 0.5, ease: "easeInOut"}],
+		]);
+		rightDesignAnimate([
+			[rightDesignScope.current, {opacity: 1}, {duration: 0.5, delay: 1.5}],
+			[rightDesignScope.current, {x:0, y:0}, {duration: 0.5}],
+		]);
+		rightPointerAnimate([
+			[rightPointerScope.current, { opacity: 1 }, {duration: 0.5, delay: 1.5}],
+			[rightPointerScope.current, { x:175, y:0 }, {duration: 0.5}],
+			[rightPointerScope.current, { x: 0, y: [0, 20, 0]}, {duration: 0.5, ease: "easeInOut"}],
 		])
+
 	},[])
 
     return (
@@ -39,12 +51,16 @@ export default function Hero() {
 								alt="Design Example 1"
 							/>
 						</motion.div>
-					<div className="absolute -right-64 -top-16 hidden lg:block">
+					<motion.div 
+						ref={rightDesignScope}
+						initial={{ opacity: 0, x: 100, y: 100 }}
+						className="absolute -right-64 -top-16 hidden lg:block"
+					>
 						<Image
 							src={designExample2Image}
 							alt="Design Example 2"
 						/>
-					</div>
+					</motion.div>
 					<motion.div 
 						ref={leftPointerScope}
 						initial={{opacity: 0, y: 100, x: -200}}
@@ -52,9 +68,13 @@ export default function Hero() {
 					>
 						<Pointer name="Andrea" />
 					</motion.div>
-					<div className="absolute right-80 -top-4 hidden lg:block">
+					<motion.div 
+						ref={rightPointerScope}
+						initial={{opacity: 0, x:275, y: 100}}
+						className="absolute right-80 -top-4 hidden lg:block"
+					>
 						<Pointer name="Bryan" color="red" />
-					</div>
+					</motion.div>
 						<div className="flex justify-center">
 							<div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
 								✨$7.5M seed round raised
